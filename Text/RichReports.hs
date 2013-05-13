18466150800926374dd270ba9d8ba94f798af11a
lapets <lapets@gmail.com>
2013-05-13 01:35:38 -0400

    Various updates to functionality; release script; built release archive.

--- Text/RichReports.hs
+++ Text/RichReports.hs
@@ -17,6 +17,7 @@ module Text.RichReports
   where
 
 import Data.List (intersperse)
+import Data.String.Utils (join)
 
 import qualified Text.Ascetic.HTML as H
 
@@ -24,16 +25,25 @@ import qualified Text.Ascetic.HTML as H
 -- Data structures (also used for report construction process)
 -- and class.
 
-type HTMLHeaders = [(String, String)]
-type CSSClass = String
-type Message = String
+type Message = Report
+
+data Highlight =
+    Unbound
+  | Duplicate
+  | Error
+  | Highlight [H.Class]
+  deriving (Eq, Show)
+
+data Category =
+    Keyword
+  | Literal
+  | Constant
+  | Variable
+  deriving (Eq, Show)
 
 data Report =
     Text String
-  | Keyword String
-  | Constant String
-  | Variable String
-  | Literal String
+  | C Category [Highlight] [Message] String
   | Space
   | Lt
   | Gt
@@ -41,12 +51,12 @@ data Report =
   | Field Report
   | Row [Report]
   | Table [Report]
-  | Span           [Message] [CSSClass] [Report]
-  | Indent                              Report
-  | Line                     [CSSClass] [Report]
-  | LineIfFlat               [CSSClass] Report
-  | Block          [Message] [CSSClass] [Report]
-  | BlockIndent    [Message] [CSSClass] [Report]
+  | Indent Report
+  | Line [String] [Report]
+  | LineIfFlat [String] Report
+  | Span [Highlight] [Message] [Report]
+  | Block [Highlight] [Message] [Report]
+  | BlockIndent [Highlight] [Message] [Report]
   | Intersperse Report [Report]
   | Finalize Report
   deriving (Show, Eq)
@@ -54,215 +64,121 @@ data Report =
 class ToReport a where
   report :: a -> Report
 
+class ToHighlights a where
+  highlights :: a -> [Highlight]
+
+class ToMessages a where
+  messages :: a -> [Message]
+
+----------------------------------------------------------------
+-- Default class members.
+
+-- None.
+
 ----------------------------------------------------------------
 -- Generation of an interactive HTML version of the report.
+
+highlight :: Highlight -> [H.Class]
+highlight h = case h of
+  Unbound -> ["RichReports_Highlight_Unbound"]
+  Duplicate -> ["RichReports_Highlight_Duplicate"]
+  Error -> ["RichReports_Highlight_Error"]
+  Highlight hs -> hs
+
+messageToAttr :: [Message] -> (H.Property, H.Value)
+messageToAttr ms = ("onclick","msg(this, [" ++ (join "," ["'" ++ show (H.html m) ++ "'" | m <- ms]) ++ "]);")
 
 instance H.ToHTML Report where
   html r = case r of
     Text s -> H.content s
+    C c hs ms s -> 
+      H.span_ 
+        (    [ ("class", 
+                "RichReports_" ++ show c 
+                ++ " " ++ (if length ms > 0 then "RichReports_Clickable" else "")
+                ++ " " ++ (if length hs > 0 then "RichReports_Highlight" else "")
+                ++ " " ++ (join " " (concat (map highlight hs)))
+                )
+             ]
+          ++ ( if length ms > 0 then [messageToAttr ms] else [] )
+        )
+        [H.content s]
     Space -> H.content "&nbsp;"
     Conc rs -> H.conc [H.html r | r <- rs]
     Field r -> H.td (H.html r)
     Row rs -> H.tr [ H.html r | r <- rs ]
     Table rs -> H.table [ H.html r | r <- rs ]
     Line _ rs -> H.div [H.html r | r <- rs]
+    Span hs ms rs ->
+      let out = H.span_ [("class", join " " (concat (map highlight hs)))] [H.html r | r <- rs]
+      in case ms of
+        [] -> out
+        ms -> H.span [H.span_ [("class","RichReports_Clickable RichReports_Clickable_Exclamation"), messageToAttr ms] [H.content "!"], out]
     Block _ _ rs -> H.div [H.html r | r <- rs]
-    BlockIndent _ _ rs -> H.divWith [("class", "RichReports_BlockIndent")] [H.html r | r <- rs]
+    BlockIndent _ _ rs -> H.div_ [("class", "RichReports_BlockIndent")] [H.html r | r <- rs]
     Intersperse r rs -> H.conc $ intersperse (H.html r) [H.html r | r <- rs]
     Finalize r -> 
       H.file 
         (H.head [
+          H.meta_ [("http-equiv","Content-type"),("content","text/html;charset=UTF-8")],
           H.style (
             H.CSS [
               ( ["body"], 
-                Nothing, 
+                Nothing,
                 [ ("font-family", "Courier,Monospace"),
                   ("font-size", "12px")
                 ]
               ),
-              ([".RichReports_BlockIndent"], Nothing, [("margin-left", "10px")])
+              ( ["table"], 
+                Nothing,
+                [ ("font-family", "Courier,Monospace"),
+                  ("font-size", "12px")
+                ]
+              ),
+              ( ["#RichReports_Message"], 
+                Nothing, 
+                [ ("background-color","yellow"),
+                  ("padding","3px"),
+                  ("border","1px solid black"),
+                  ("font-family", "Courier,Monospace"),
+                  ("font-size", "12px")
+                ]
+              ),
+              ([".RichReports_Clickable"], Nothing, [("cursor","pointer")]),
+              ( [".RichReports_Clickable_Exclamation"], 
+                Nothing, 
+                [("background-color","yellow"), ("border","1px solid black"), ("margin","0px 5px 0px 5px")]
+              ),
+              ([".RichReports_Clickable"], Just "hover", [("background-color","yellow")]),
+              ([".RichReports_BlockIndent"], Nothing, [("margin-left", "10px")]),
+              ([".RichReports_Keyword"], Nothing, [("font-weight", "bold"), ("color", "blue")]),
+              ([".RichReports_Variable"], Nothing, [("font-style", "italic"), ("color", "green")]),
+              ([".RichReports_Literal"], Nothing, [("font-weight", "bold"), ("color", "firebrick")]),
+              ([".RichReports_Highlight"], Nothing, [("margin","2px")]),
+              ([".RichReports_Highlight_Unbound"], Nothing, [("background-color", "orange")]),
+              ([".RichReports_Highlight_Duplicate"], Nothing, [("background-color", "yellow")]),
+              ([".RichReports_Highlight_Error"], Nothing, [("background-color", "lightpink")])
             ]
           ),
-          H.script ""
+          H.script_ [("type","text/javascript"), ("src","http://ajax.googleapis.com/ajax/libs/jquery/1.4.3/jquery.min.js")] "",
+          H.script $
+            "function msg (obj, msgs) {"
+              ++ "var html = '';"
+              ++ "for (var i = 0; i < msgs.length; i++) html += '<div class=\"RichReports_MessagePortion\">' + msgs[i] + '</div>';"
+              ++ "document.getElementById('RichReports_Message').innerHTML = html;"
+              ++ "document.getElementById('RichReports_Message').style.display = 'inline-block';"
+              ++ "var top = $(obj).offset().top;"
+              ++ "var left = $(obj).offset().left;"
+              ++ "$('#RichReports_Message').offset({top:top + 15, left:left + 15});"
+              ++ "}"
         ])
-        (H.body [H.html r])
+        (H.body [
+          H.html r,
+          H.div_ [("id","RichReports_Message"), ("style","display:none;"), ("onclick", "this.style.display='none';")] [H.content ""]
+        ])
     
     
     
     _ -> H.content ""
-
-
-
-
-
-
-
-
-
-
-
-{-
-
-
-reportHTML :: Report -> String
-reportHTML r = showConcreteRec 0 (annotLeavesOnly r) where
-  showConcreteRec i r = let sh = showConcreteRec i in case r of
-    Text s               -> s
-    Space                -> " "
-    Lt                   -> "&lt;"
-    Gt                   -> "&gt;"
-    Span        ms cs rs -> elementWithMsgs "span" ms cs $ concat (map sh rs)
-    Indent            r  -> showConcreteRec (i+2) r
-    Line           cs rs -> element "div"   cs $ concat (map sh $ [Text (concat $ take i (repeat "&nbsp;"))] ++ rs)
-    Block       ms cs rs -> elementWithMsgs "div" ms cs $ concat (map sh rs)
-    BlockIndent ms cs rs -> elementWithMsgs "div" ms cs $ concat (map (showConcreteRec (i+2)) rs)
-    Conc              rs -> concat $ map sh rs
-
-
-
-
-----------------------------------------------------------------
--- Constraints that must be satisfied by a data structure that
--- can be converted into an HTML representation of a concrete
--- syntax.
-
-class ToConcrete a where
-  concrete :: a -> Concrete
-
-instance ToConcrete Char where
-  concrete c = Text $ show c
-
-instance ToConcrete Int where
-  concrete n = Text $ show n
-
-instance ToConcrete Integer where
-  concrete n = Text $ show n
-
-instance ToConcrete a => ToConcrete [a] where
-  concrete xs = Conc $ intersperse (Text ",") $ map concrete xs
-
-instance ToConcrete (HaXml.Element i) where
-  concrete e = Text (HaXml.render (HaXml.element e))
-
-----------------------------------------------------------------
--- Convenient functions for inspecting and modifying an
--- instance.
-
-isFlat :: Concrete -> Bool
-isFlat r = case r of
-  Text       _   -> True
-  Space          -> True
-  Lt             -> True
-  Gt             -> True
-  Indent       _ -> True
-  Line       _ _ -> True
-  LineIfFlat _ _ -> True
-  Span     _ _ _ -> True
-  Conc         _ -> True
-  _              -> False
-
-hasMsg :: Concrete -> Bool
-hasMsg r = case r of
-  Indent h             -> hasMsg h
-  Conc hs              -> or (map hasMsg hs)
-  Span ms cs hs        -> length ms > 0 || or (map hasMsg hs)
-  Line cs hs           -> or (map hasMsg hs)
-  LineIfFlat cs h      -> hasMsg h
-  Block ms cs hs       -> length ms > 0 || or (map hasMsg hs)
-  BlockIndent ms cs hs -> length ms > 0 || or (map hasMsg hs)
-  Field cs h           -> hasMsg h
-  Row cs hs            -> or (map hasMsg hs)
-  Table cs hs          -> or (map hasMsg hs)
-  _                    -> False
-
-annotLeavesOnly :: Concrete -> Concrete
-annotLeavesOnly h = 
-  let mlo h = case h of
-        Indent h             -> Indent $ mlo h
-        Conc hs              -> Conc $ map mlo hs
-        Line cs hs           -> Line cs $ map mlo hs
-        LineIfFlat cs h      -> LineIfFlat cs $ mlo h
-        Field cs h           -> Field cs $ mlo h
-        Row cs hs            -> Row cs $ map mlo hs
-        Table cs hs          -> Table cs $ map mlo hs
-        Block ms cs hs       -> let hs'=map mlo hs; b=or(map hasMsg hs') in Block       (if b then [] else ms) cs hs'
-        BlockIndent ms cs hs -> let hs'=map mlo hs; b=or(map hasMsg hs') in BlockIndent (if b then [] else ms) cs hs'
-        Span ms cs hs        -> let hs'=map mlo hs; b=or(map hasMsg hs') in Span        (if b then [] else ms) cs hs'
-        _                    -> h
-  in mlo h
-
-annotRootsOnly :: Concrete -> Concrete
-annotRootsOnly h = 
-  let mlo b h = case h of
-        Indent h             -> Indent $ mlo b h
-        Conc hs              -> Conc $ map (mlo b) hs
-        Line cs hs           -> Line cs $ map (mlo b) hs
-        LineIfFlat cs h      -> LineIfFlat cs $ mlo b h
-        Field cs h           -> Field cs $ mlo b h
-        Row cs hs            -> Row cs $ map (mlo b) hs
-        Table cs hs          -> Table cs $ map (mlo b) hs
-        Block ms cs hs       -> Block       (if b then ms else []) (if b then cs else []) $ map (if ms == [] then mlo b else mlo False) hs
-        BlockIndent ms cs hs -> BlockIndent (if b then ms else []) (if b then cs else []) $ map (if ms == [] then mlo b else mlo False) hs
-        Span ms cs hs        -> Span        (if b then ms else []) (if b then cs else []) $ map (if ms == [] then mlo b else mlo False) hs
-        _                    -> h
-  in mlo True h
-
-----------------------------------------------------------------
--- Conversion of an instance to a string.
-
-fixTextToFormat :: String -> String
-fixTextToFormat s = case s of
-  (' ':s)  -> "&nbsp;" ++ fixTextToFormat s
-  ('\n':s) -> "<br/>" ++ fixTextToFormat s
-  (c:s)    -> c : fixTextToFormat s
-  "" -> ""
-
-element :: String -> [CSSClass] -> String -> String
-element tag [] s = "<" ++ tag ++ ">" ++ s ++ "</" ++ tag ++ ">"
-element tag cs s = "<" ++ tag ++ " class=\"" ++ (join " " cs) ++ "\">" ++ s ++ "</" ++ tag ++ ">"
-
-elementWithMsgs :: String -> [Message] -> [CSSClass] -> String -> String
-elementWithMsgs tag [] [] s = "<" ++ tag ++ ">" ++ s ++ "</" ++ tag ++ ">"
-elementWithMsgs tag [] cs s = "<" ++ tag ++ " class=\"" ++ (join " " cs) ++ "\">" ++ s ++ "</" ++ tag ++ ">"
-elementWithMsgs tag ms cs s = 
-     "<" ++ tag 
-         ++ " onclick=\"msg(this, [" ++ (join ", " (["'"++m++"'"|m<-ms])) ++ "]);\""
-         ++ " class=\"" ++ (join " " cs) ++ " " ++ tag ++ "_msg\">" 
-  ++ s 
-  ++ "</" ++ tag ++ ">"
-
-tag :: String -> String -> String
-tag t s = element t [] s
-
-tagCDATA :: String -> String -> String
-tagCDATA t s = element t [] $ "<![CDATA[" ++ s ++ "]]>"    
-
-
-showASCII :: Concrete -> String
-showASCII r = showASCIIRec 0 (annotLeavesOnly r) where
-  showASCIIRec i r = let sh = showASCIIRec i in case r of
-    ImgScrollZoom _ _    -> "[image]"
-    Text s               -> s
-    Space                -> " " 
-    Lt                   -> "<"
-    Gt                   -> ">"
-    Indent            r  -> showASCIIRec (i+2) r
-    Table          cs rs -> concat (map sh rs)
-    Row            cs rs -> "\n" ++ concat (map sh rs)
-    Field          cs r  -> sh r
-    Block       ms cs rs -> concat (map sh rs)
-    BlockIndent ms cs rs -> concat (map (showASCIIRec (i+2)) rs)
-    Line           cs rs -> "\n" ++ concat (map sh $ [Text (concat $ take i (repeat " "))] ++ rs)
-    LineIfFlat     cs r  -> if isFlat r then "\n" ++ concat (map sh $ [Text (concat $ take i (repeat " "))] ++ [r]) else sh r
-    Span        ms cs rs -> concat $ map sh rs
-    Conc              rs -> concat $ map sh rs
-
-mkLineIfFlat clss c = case c of
-  (LineIfFlat clss' c) -> (LineIfFlat (clss'++clss) c)
-  c -> LineIfFlat clss c
-
-mkLineIfFlats clss cs = [mkLineIfFlat [] c | c <- cs]
-
--}
 
 --eof
